8ff489d16cc54a4e135195d8f6e2d39aa91b5ee6
githong <githong@kakao.com>
2021-10-15 18:02:17 +0900

    initial commit

---
+++ nextjs-blog/pages/404.js
@@ -0,0 +1,11 @@
+import utilStyles from "../styles/utils.module.css";
+
+// pages/404.js
+export default function Custom404() {
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+      <h1 className={utilStyles.headingLg}>Contact to administrator</h1>
+    </>
+  );
+}
